6f3813412188e20bb2662759e841339150cb0fbc
chrisarme <christopher.arme@gmail.com>
2018-02-14 11:52:30 -0600

    Worked on project 4

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	@0
+	D=M
+	@END
+	D;JEQ
+	
+	@1
+	D=M
+	@END
+	D;JEQ
+	
+	@2
+	M=0
+	
+	@1
+	D=M
+	@3
+	M=D
+
+(LOOP)
+	
+	@0
+	D=M
+	
+	@sum
+	M=M+D
+	
+	@3
+	M=M-1
+	
+	D=M
+	@LOOP
+	D;JGT
+(END)
+	@END
+	0;JMP
