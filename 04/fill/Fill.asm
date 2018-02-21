b9a6a8dd705d983a426503b95e4e2dae69f784c5
chrisarme <christopher.arme@gmail.com>
2018-02-21 12:04:03 -0600

    Finished Fill.hack
    Needed some online help for this one, but I understand how it works now.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,63 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(BEGINNING)
+
+// Put screen into memory
+@SCREEN
+D=A
+@0
+M=D
+
+(KBDCHECK)
+
+@KBD
+D=M
+@BLACK
+D; JGT
+@WHITE
+D; JEQ
+
+@KBDCHECK // If it gets here, it needs to check to loop again because it should never get here
+0; JMP
+
+(WHITE)
+@1
+M=0 // The color will equal 0000...
+@CHANGELOOP
+0; JMP
+
+(BLACK)
+@1
+M=-1 // The color will equal 1111...
+@CHANGELOOP
+0; JMP
+
+
+// Now change each pixel!
+
+(CHANGELOOP)
+
+@1
+D=M // get what the pixel should be (black/white)
+
+@0
+A=M
+M=D // change the pixel to whatever it should be
+
+@0 // Still at 0, just making sure
+D=M+1 // inc pixel chosen
+@KBD
+D=A-D
+
+@0
+M=M+1
+A=M
+
+
+@CHANGELOOP
+A; JGT
+
+@BEGINNING
+0; JMP
